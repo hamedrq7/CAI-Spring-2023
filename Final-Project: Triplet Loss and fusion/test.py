d9de5f3a9037a5255b39298f3a600db50610375c
hamed <hamedmuhammadzadeh1380@gmail.com>
2023-12-03 21:02:56 +0330

    ++

--- Final-Project: Triplet Loss and fusion/test.py
+++
@@ -1,8 +0,0 @@
-
-import argparse
-parser = argparse.ArgumentParser(description='vis')
-
-parser.add_argument('-l','--domains', nargs='+', help='<Required> Set flag', required=True)
-args = parser.parse_args()
-
-print(args)
